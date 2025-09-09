21d2ffb69c291690f5f3bb0b8ba0986ab69998a9
Tanmoy Giri <tanmoygiri333@gmail.com>
2025-09-09 10:55:02 +0530

    add new problems

--- gitpush.bat
+++ gitpush.bat
@@ -1,7 +1,4 @@
 @echo off
-REM --------------------------------------
-REM Batch script to commit and push changes
-REM --------------------------------------
 
 cd /d %~dp0
 
@@ -10,5 +7,5 @@ git commit -m "add new problems"
 git push
 
 echo.
-echo ✅ All changes pushed successfully!
+echo All changes pushed successfully!
 pause
